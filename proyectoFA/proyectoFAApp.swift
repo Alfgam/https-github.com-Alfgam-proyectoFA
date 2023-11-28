7356e353326873004ac813186ecb4bc9be997a9a
Alfonso <znipsgam@gmail.com>
2023-11-27 18:37:02 -0600

    Initial Commit

---
+++ proyectoFA/proyectoFAApp.swift
@@ -0,0 +1,17 @@
+//
+//  proyectoFAApp.swift
+//  proyectoFA
+//
+//  Created by Alfonso Gamboa  on 27/11/23.
+//
+
+import SwiftUI
+
+@main
+struct proyectoFAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
